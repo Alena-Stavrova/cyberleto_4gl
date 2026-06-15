a8c648ac4984b6e0dd3d84d46b5229fba37790c9
Alena Stavrova <33433876+Alena-Stavrova@users.noreply.github.com>
2026-06-15 15:33:25 +0300

    Update promo_checker.py
    Working on a promo checker, still work in progress

--- promo_checker.py
+++ promo_checker.py
@@ -1,6 +1,10 @@
 from openpyxl import load_workbook
+import math
 
-workbook = load_workbook("item_list.xlsx")
+def round_price(value):
+    return math.floor(value + 0.5)
+
+workbook = load_workbook("item_list.xlsx", data_only=True)
 sheet = workbook.active  
 
 # sheet[1] is row 1. Each cell has a .value attribute.
@@ -11,6 +15,7 @@ old_price_col = headers.index("Текущая цена, руб.")
 discount_col= headers.index("Скидка")
 new_price_col = headers.index("Конечная цена")
 
+
 items = []
 for row in sheet.iter_rows(min_row=2, values_only=True):
     items.append({
@@ -18,7 +23,10 @@ for row in sheet.iter_rows(min_row=2, values_only=True):
         'name': row[name_col],
         'old_price': str(row[old_price_col]),
         'discount' : str(row[discount_col]),
-        'new_price' : str(row[new_price_col])
+        'new_price': str(round_price(row[new_price_col]))
+        #'new_price' : str(extract_price(row[new_price_col]))
     })
 print(items)
+print(len(items))
+print(items[0]['discount'], items[1]['discount'], items[2]['discount'])
 
